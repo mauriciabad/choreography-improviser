1ceee32989fcaf5c1c796849c98142dfbb57c042
Maurici Abad <maurici.abad@glovoapp.com>
2021-05-23 14:59:12 +0200

    Update prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,40 @@
+/**
+ * @type {import("eslint").Linter.Config}
+ */
 module.exports = {
   root: true,
   env: {
-    node: true,
+    browser: true,
   },
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint',
   ],
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2021,
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'prettier/prettier': ['error', require('./prettier.config.js')],
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/order-in-components': 'error',
+    'vue/attributes-order': 'error',
+    'vue/no-v-html': 'error',
+    'prefer-template': 'warn',
   },
-};
+  overrides: [
+    {
+      files: ['*.js', '*.ts'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+      env: {
+        node: true,
+      },
+    },
+  ],
+  ignorePatterns: ['/dist/'],
+}
